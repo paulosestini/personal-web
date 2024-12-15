0ed6d07450d250781ddbc8efd181e639afd48025
paulosestini <24728744+paulosestini@users.noreply.github.com>
2024-12-15 15:50:00 -0300

    website

--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/")
+def home():
+    return send_from_directory("../src", "index.html")
+
+
+@app.route("/<path:filename>")
+def serve_html(filename):
+    if len(filename.split(".")) == 1:
+        filename += ".html"
+    return send_from_directory("../src", filename)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
